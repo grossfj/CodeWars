b8c6784308ac0f261551949d1dee6d3fb2743d1d
grossfj <94851595+grossfj@users.noreply.github.com>
2022-12-16 23:23:46 +0300

    Array

---
+++ js4.js
@@ -0,0 +1,20 @@
+function getLength(arr) {
+    return arr.length
+}
+function getFirst(arr) {
+    var one = arr[0]
+    return one
+}
+function getLast(arr) {
+    var neone = arr[arr.length - 1]
+    return neone
+}
+function pushElement(arr) {
+    var el = 1;
+    arr.push(el)
+    return arr
+}
+function popElement(arr) {
+    arr.pop()
+    return arr
+}
