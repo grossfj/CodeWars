dc5880ac22b39cf3732baa963cc5f3a0f5ee280d
grossfj <94851595+grossfj@users.noreply.github.com>
2022-12-25 23:46:41 +0300

    colorOf

---
+++ js14.js
@@ -0,0 +1,19 @@
+function colorOf(r, g, b) {
+
+    let R = r.toString(16);
+    let G = g.toString(16);
+    let B = b.toString(16);
+
+    if (R.length < 2) {
+        R = "0" + R;
+    }
+    if (G.length < 2) {
+        G = "0" + G;
+    }
+    if (B.length < 2) {
+        B = "0" + B;
+    }
+
+    return `#${R}${G}${B}`
+
+}
