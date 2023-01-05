50ee262751979d2589b78723b0622c0333ed7dea
grossfj <94851595+grossfj@users.noreply.github.com>
2023-01-05 23:35:18 +0300

    sumArray

---
+++ js24.js
@@ -0,0 +1,21 @@
+function sumArray(array) {
+    //add conditionals to check if the array is empty, null, or none, or if it only has 1 element, if this is the case return 0
+
+    if (array == null) {
+        return 0;
+    }
+    else if (array.length < 2) {
+        return 0;
+    }
+    else {
+
+        array = array.sort(function (a, b) { return a - b; });
+        //         variable to hold the sum of the sorted array
+        var total = 0;
+
+        for (var i = 1; i < array.length - 1; i++) {
+            total += array[i];
+        }
+        return total;
+    }
+}
