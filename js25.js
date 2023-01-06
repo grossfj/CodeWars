1a850e32f91e40bb8d12cf80732c051896a3f171
grossfj <94851595+grossfj@users.noreply.github.com>
2023-01-06 22:36:46 +0300

    makeNegative

---
+++ js25.js
@@ -0,0 +1,11 @@
+function makeNegative(num) {
+    // Code?
+    if (num == 0) {
+        return num;
+    } else if (num < 0) {
+        return num;
+    } else if (num > 0) {
+        return num = - (num);
+    }
+
+}
