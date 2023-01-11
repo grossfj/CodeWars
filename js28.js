8256c7a582bb102a45a8ef67ca701c17e9d24cef
grossfj <94851595+grossfj@users.noreply.github.com>
2023-01-11 12:04:39 +0300

    reverseWord

---
+++ js28.js
@@ -0,0 +1,8 @@
+function reverseWords(str) {
+    var stra = str.split(' ');
+    console.log(str);
+    stra.reverse();
+    stra = stra.join(' ');
+    return stra;
+    // reverse those words
+}  
