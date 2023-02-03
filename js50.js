dcf7fb4ddfef51d11b404511e3cc2f8093ae9b9c
grossfj <94851595+grossfj@users.noreply.github.com>
2023-02-03 23:29:40 +0300

    lovefunc

---
+++ js50.js
@@ -0,0 +1,9 @@
+function lovefunc(flower1, flower2) {
+    // moment of truth
+    let sum = flower1 + flower2;
+    if (sum % 2) {
+        return true;
+    } else {
+        return false;
+    }
+}
